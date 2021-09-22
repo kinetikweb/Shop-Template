f098699e666ce4a06ed805d299380ee4e563f6c3
kinetikweb <kinetik2017@gmail.com>
2021-09-22 17:01:58 +0300

    Customizing Container

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
 <template>
-    <!DOCTYPE html>
-    <html lang="en" dir="ltr">
-        <head>
-            <meta charset="utf-8">
-            <title>Tailwind E-commerce</title>
-        </head>
-        <body>
-            <h1 class="font-roboto">
-                I'm roboto
-            </h1>
-            <h1 class="font-poppins">
-                I'm poppins
-            </h1>
-        </body>
-    </html>
+    <div class="container h-20 bg-gray-300">
+
+    </div>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,30 @@
 
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
-  mode: 'jit',
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        poppins: "'Poppins', sans-serif",
-        roboto: "'Roboto', sans-serif",
-      },
+    mode: 'jit',
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        screens: {
+            sm: '576px',
+            md: '768px',
+            lg: '992px',
+            xl: '1200px',
+        },
+        container: {
+            center: true,
+            padding: '1rem',
+        },
+        extend: {
+            fontFamily: {
+                poppins: "'Poppins', sans-serif",
+                roboto: "'Roboto', sans-serif",
+            },
+        },
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
 }
