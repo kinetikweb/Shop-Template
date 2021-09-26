8c2b4ef6f2bfa91f4687402c83155984092284fd
kinetikweb <kinetik2017@gmail.com>
2021-09-26 22:26:45 +0300

    Features

---
+++ src/components/Features.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container py-16">
+        <div class="w-10/12 grid grid-cols-3 gap-6 mx-auto justify-center ">
+            <!-- single feature -->
+            <div class="border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5">
+                <img src="@/assets/images/free-shipping.png" class="w-12 h-12 object-cover" alt="no image">
+                <div class="">
+                    <h4 class="font-medium capitalize text-lg">
+                        Free shipping
+                    </h4>
+                    <p class="text-gray-500 text-sm">
+                        order over $200
+                    </p>
+                </div>
+            </div>
+            <!-- end single feature -->
+            <div class="border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5">
+                <img src="@/assets/images/return-on-investment.png" class="w-12 h-12 object-cover" alt="no image">
+                <div class="">
+                    <h4 class="font-medium capitalize text-lg">
+                        Money returns
+                    </h4>
+                    <p class="text-gray-500 text-sm">
+                        30 days money returns
+                    </p>
+                </div>
+            </div>
+            <div class="border border-primary rounded-sm px-3 py-6 flex justify-center items-center gap-5">
+                <img src="@/assets/images/24-hours.png" class="w-12 h-12 object-cover" alt="no image">
+                <div class="">
+                    <h4 class="font-medium capitalize text-lg">
+                        24/7 support
+                    </h4>
+                    <p class="text-gray-500 text-sm">
+                        Customer support
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
     <Banner />
+    <Features />
 </template>
 
 <script setup>
 import Banner from '@/components/Banner.vue'
+import Features from '@/components/Features.vue'
 </script>
